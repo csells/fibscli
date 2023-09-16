29e5a622d9f651f452ff486d54e3b580b5cadc2e
Chris Sells <csells@sellsbrothers.com>
2023-09-16 13:36:40 -0700

    migrated to null safety

--- lib/tinystate.dart
+++ lib/tinystate.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:dartx/dartx.dart';
 
 class ChangeNotifierBuilder<T extends ChangeNotifier> extends AnimatedBuilder {
--- lib/who_page.dart
+++ lib/who_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:syncfusion_flutter_datagrid/datagrid.dart';
+//import 'package:syncfusion_flutter_datagrid/datagrid.dart';
 
 import 'package:fibscli/fibs_state.dart';
 import 'package:fibscli/main.dart';
@@ -12,7 +12,7 @@ class WhoPage extends StatefulWidget {
 
 class _WhoPageState extends State<WhoPage> {
   var _showMessages = false;
-  var _source = WhoDataSource(App.fibs.whoInfos, filter: 'both');
+  // var _source = WhoDataSource(App.fibs.whoInfos, filter: 'both');
 
   @override
   Widget build(BuildContext context) => Scaffold(
@@ -30,7 +30,8 @@ class _WhoPageState extends State<WhoPage> {
               icon: Icon(Icons.send),
               tooltip: 'send command',
             ),
-            OutlineButton(onPressed: () => App.fibs.logout(), child: Text('Logout')),
+            OutlinedButton(
+                onPressed: () => App.fibs.logout(), child: Text('Logout')),
           ],
         ),
         body: Row(
@@ -52,39 +53,61 @@ class _WhoPageState extends State<WhoPage> {
                                 child: Text(
                                   'FIBS Who',
                                   style: TextStyle(
-                                      color: Theme.of(context).primaryColor, fontWeight: FontWeight.w500, fontSize: 36),
+                                      color: Theme.of(context).primaryColor,
+                                      fontWeight: FontWeight.w500,
+                                      fontSize: 36),
                                 ),
                               ),
                               Align(
                                 alignment: Alignment.centerRight,
-                                child: DropdownButton<String>(
-                                  value: _source.filter,
-                                  items: [
-                                    for (final item in ['both', 'humans', 'bots'])
-                                      DropdownMenuItem<String>(value: item, child: Text(item)),
-                                  ],
-                                  onChanged: (item) => setState(() => _source.filter = item),
+                                child: Container(
+                                  child: Text('TODO'),
                                 ),
+                                // TODO
+                                // DropdownButton<String>(
+                                //   value: _source.filter,
+                                //   items: [
+                                //     for (final item in [
+                                //       'both',
+                                //       'humans',
+                                //       'bots'
+                                //     ])
+                                //       DropdownMenuItem<String>(
+                                //           value: item, child: Text(item)),
+                                //   ],
+                                //   onChanged: (item) =>
+                                //       setState(() => _source.filter = item),
+                                // ),
                               )
                             ],
                           ),
                         ),
                         Expanded(
-                          child: SfDataGrid(
-                            source: _source,
-                            columnWidthMode: ColumnWidthMode.fill,
-                            allowMultiColumnSorting: true,
-                            allowSorting: true,
-                            allowTriStateSorting: true,
-                            onCellTap: _tapCell,
-                            columns: <GridColumn>[
-                              GridTextColumn(mappingName: 'user', headerText: 'user'),
-                              GridNumericColumn(mappingName: 'experience', headerText: 'experience'),
-                              GridTextColumn(mappingName: 'opponent', headerText: 'opponent'),
-                              GridNumericColumn(mappingName: 'rating', headerText: 'rating'),
-                              GridTextColumn(mappingName: 'ready', headerText: 'ready'),
-                            ],
+                          child: Container(
+                            child: Text('TODO'),
                           ),
+                          // SfDataGrid(
+                          //   source: _source,
+                          //   columnWidthMode: ColumnWidthMode.fill,
+                          //   allowMultiColumnSorting: true,
+                          //   allowSorting: true,
+                          //   allowTriStateSorting: true,
+                          //   onCellTap: _tapCell,
+                          //   columns: <GridColumn>[
+                          //     GridColumn(
+                          //         columnName: 'user', label: Text('user')),
+                          //     GridColumn(
+                          //         columnName: 'experience',
+                          //         label: Text('experience')),
+                          //     GridColumn(
+                          //         columnName: 'opponent',
+                          //         label: Text('opponent')),
+                          //     GridColumn(
+                          //         columnName: 'rating', label: Text('rating')),
+                          //     GridColumn(
+                          //         columnName: 'ready', label: Text('ready')),
+                          //   ],
+                          // ),
                         ),
                       ],
                     ),
@@ -97,74 +120,91 @@ class _WhoPageState extends State<WhoPage> {
         ),
       );
 
-  void _watch(WhoInfo who) {
-    assert(who != null);
-    print('TODO: watch ${who.user}');
-  }
+  // TODO
+  // void _watch(WhoInfo who) {
+  //   assert(who != null);
+  //   print('TODO: watch ${who.user}');
+  // }
 
-  void _invite(WhoInfo who) {
-    assert(who != null);
-    App.fibs.invite(who, 1);
-  }
+  // TODO
+  // void _invite(WhoInfo who) {
+  //   assert(who != null);
+  //   App.fibs.invite(who, 1);
+  // }
 
-  void _tapWho(BuildContext context, WhoInfo who) async {
-    await showDialog<void>(
-      context: context,
-      builder: (context) => AlertDialog(
-        title: Text('Who Info'),
-        actions: [
-          OutlineButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            child: Text('Close'),
-          ),
-          if (who.user != App.fibs.user && who.opponent.isNotEmpty)
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context);
-                _watch(who);
-              },
-              child: Text('Watch'),
-            ),
-          if (who.user != App.fibs.user && who.opponent.isEmpty && who.ready)
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context);
-                _invite(who);
-              },
-              child: Text('Invite'),
-            ),
-        ],
-        content: Container(
-          width: 500,
-          child: Table(
-            columnWidths: {0: FixedColumnWidth(100)},
-            children: [
-              TableRow(children: [Text('user'), Text(who.user)]),
-              TableRow(children: [Text('away'), Text(who.away.toString())]),
-              TableRow(children: [Text('client'), Text(who.client)]),
-              TableRow(children: [Text('email'), Text(who.email)]),
-              TableRow(children: [Text('experience'), Text(who.experience.toString())]),
-              TableRow(children: [Text('hostname'), Text(who.hostname)]),
-              TableRow(children: [Text('last active'), Text(who.lastActive.toString())]),
-              TableRow(children: [Text('last login'), Text(who.lastLogin.toString())]),
-              TableRow(children: [Text('opponent'), Text(who.opponent)]),
-              TableRow(children: [Text('rating'), Text(who.rating.toStringAsFixed(2))]),
-              TableRow(children: [Text('ready'), Text(who.ready.toString())]),
-              TableRow(children: [Text('watching'), Text(who.watching)]),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
+  // TODO
+  // void _tapWho(BuildContext context, WhoInfo who) async {
+  //   await showDialog<void>(
+  //     context: context,
+  //     builder: (context) => AlertDialog(
+  //       title: Text('Who Info'),
+  //       actions: [
+  //         OutlinedButton(
+  //           onPressed: () {
+  //             Navigator.pop(context);
+  //           },
+  //           child: Text('Close'),
+  //         ),
+  //         if (who.user != App.fibs.user && who.opponent.isNotEmpty)
+  //           ElevatedButton(
+  //             onPressed: () {
+  //               Navigator.pop(context);
+  //               _watch(who);
+  //             },
+  //             child: Text('Watch'),
+  //           ),
+  //         if (who.user != App.fibs.user && who.opponent.isEmpty && who.ready)
+  //           ElevatedButton(
+  //             onPressed: () {
+  //               Navigator.pop(context);
+  //               _invite(who);
+  //             },
+  //             child: Text('Invite'),
+  //           ),
+  //       ],
+  //       content: Container(
+  //         width: 500,
+  //         child: Table(
+  //           columnWidths: {0: FixedColumnWidth(100)},
+  //           children: [
+  //             TableRow(children: [Text('user'), Text(who.user)]),
+  //             TableRow(children: [Text('away'), Text(who.away.toString())]),
+  //             TableRow(children: [Text('client'), Text(who.client)]),
+  //             TableRow(children: [Text('email'), Text(who.email)]),
+  //             TableRow(children: [
+  //               Text('experience'),
+  //               Text(who.experience.toString())
+  //             ]),
+  //             TableRow(children: [Text('hostname'), Text(who.hostname)]),
+  //             TableRow(children: [
+  //               Text('last active'),
+  //               Text(who.lastActive.toString())
+  //             ]),
+  //             TableRow(children: [
+  //               Text('last login'),
+  //               Text(who.lastLogin.toString())
+  //             ]),
+  //             TableRow(children: [Text('opponent'), Text(who.opponent)]),
+  //             TableRow(children: [
+  //               Text('rating'),
+  //               Text(who.rating.toStringAsFixed(2))
+  //             ]),
+  //             TableRow(children: [Text('ready'), Text(who.ready.toString())]),
+  //             TableRow(children: [Text('watching'), Text(who.watching)]),
+  //           ],
+  //         ),
+  //       ),
+  //     ),
+  //   );
+  // }
 
-  void _tapCell(DataGridCellTapDetails details) {
-    if (details.rowColumnIndex.rowIndex == 0) return; // header
-    final who = _source.getCellValue(details.rowColumnIndex.rowIndex - 1, '') as WhoInfo;
-    _tapWho(context, who);
-  }
+  // TODO
+  // void _tapCell(DataGridCellTapDetails details) {
+  //   if (details.rowColumnIndex.rowIndex == 0) return; // header
+  //   final who = _source.getCellValue(details.rowColumnIndex.rowIndex - 1, '')
+  //       as WhoInfo;
+  //   _tapWho(context, who);
+  // }
 
   void _tapSend(BuildContext context) async {
     final cmd = await SendComandDialog.getCommand(context);
@@ -173,7 +213,8 @@ class _WhoPageState extends State<WhoPage> {
 }
 
 class SendComandDialog extends StatefulWidget {
-  static Future<String> getCommand(BuildContext context) async => await showDialog<String>(
+  static Future<String> getCommand(BuildContext context) async =>
+      await showDialog<String>(
         context: context,
         builder: (context) => Dialog(child: SendComandDialog()),
       );
@@ -197,83 +238,98 @@ class _SendComandDialogState extends State<SendComandDialog> {
         child: Row(
           children: [
             Expanded(
-                child: TextField(decoration: InputDecoration(hintText: "command to send"), controller: _controller)),
-            OutlineButton(onPressed: () => Navigator.pop(context, ''), child: Text('Cancel')),
+                child: TextField(
+                    decoration: InputDecoration(hintText: "command to send"),
+                    controller: _controller)),
+            OutlinedButton(
+                onPressed: () => Navigator.pop(context, ''),
+                child: Text('Cancel')),
             SizedBox(width: 8),
-            ElevatedButton(onPressed: () => Navigator.pop(context, _controller.text), child: Text('Send')),
+            ElevatedButton(
+                onPressed: () => Navigator.pop(context, _controller.text),
+                child: Text('Send')),
           ],
         ),
       );
 }
 
-class WhoDataSource extends DataGridSource<WhoInfo> {
-  final NotifierList<WhoInfo> whoInfos;
-  String _filter;
-  WhoDataSource(this.whoInfos, {String filter = 'both'})
-      : _filter = filter,
-        assert(whoInfos != null) {
-    whoInfos.addListener(notifyDataSourceListeners);
-  }
+// TODO
+// class WhoDataSource extends DataGridSource<WhoInfo> {
+//   final NotifierList<WhoInfo> whoInfos;
+//   String _filter;
+//   WhoDataSource(this.whoInfos, {String filter = 'both'})
+//       : _filter = filter,
+//         assert(whoInfos != null) {
+//     whoInfos.addListener(notifyDataSourceListeners);
+//   }
 
-  @override
-  void dispose() {
-    whoInfos.removeListener(notifyDataSourceListeners);
-    super.dispose();
-  }
+//   @override
+//   void dispose() {
+//     whoInfos.removeListener(notifyDataSourceListeners);
+//     super.dispose();
+//   }
 
-  String get filter => _filter;
-  set filter(String filter) {
-    _filter = filter;
-    notifyDataSourceListeners();
-  }
+//   String get filter => _filter;
+//   set filter(String filter) {
+//     _filter = filter;
+//     notifyDataSourceListeners();
+//   }
 
-  static List<WhoInfo> _filtered(NotifierList<WhoInfo> whoInfos, String filter) {
-    bool bot(String user) => user.contains('Bot');
+//   static List<WhoInfo> _filtered(
+//       NotifierList<WhoInfo> whoInfos, String filter) {
+//     bool bot(String user) => user.contains('Bot');
 
-    switch (filter) {
-      case 'both':
-        return whoInfos.toList();
-      case 'humans':
-        return whoInfos.where((wi) => !bot(wi.user)).toList();
-      case 'bots':
-        return whoInfos.where((wi) => bot(wi.user)).toList();
-      default:
-        throw 'unreachable: filter= $filter';
-    }
-  }
+//     switch (filter) {
+//       case 'both':
+//         return whoInfos.toList();
+//       case 'humans':
+//         return whoInfos.where((wi) => !bot(wi.user)).toList();
+//       case 'bots':
+//         return whoInfos.where((wi) => bot(wi.user)).toList();
+//       default:
+//         throw 'unreachable: filter= $filter';
+//     }
+//   }
 
-  @override
-  List<WhoInfo> get dataSource => _filtered(whoInfos, _filter);
+//   @override
+//   List<WhoInfo> get dataSource => _filtered(whoInfos, _filter);
 
-  @override
-  Object getValue(WhoInfo whoInfo, String columnName) {
-    switch (columnName) {
-      case 'user':
-        return whoInfo.user;
-      case 'away':
-        return whoInfo.away.toString();
-      case 'experience':
-        return whoInfo.experience;
-      case 'opponent':
-        return whoInfo.opponent;
-      case 'rating':
-        return whoInfo.rating;
-      case 'ready':
-        return whoInfo.ready.toString();
-      case '':
-        return whoInfo;
-      default:
-        throw 'unreachable: columnName= $columnName';
-    }
-  }
+//   @override
+//   Object getValue(WhoInfo whoInfo, String columnName) {
+//     switch (columnName) {
+//       case 'user':
+//         return whoInfo.user;
+//       case 'away':
+//         return whoInfo.away.toString();
+//       case 'experience':
+//         return whoInfo.experience;
+//       case 'opponent':
+//         return whoInfo.opponent;
+//       case 'rating':
+//         return whoInfo.rating;
+//       case 'ready':
+//         return whoInfo.ready.toString();
+//       case '':
+//         return whoInfo;
+//       default:
+//         throw 'unreachable: columnName= $columnName';
+//     }
+//   }
 
-  @override
-  int compare(WhoInfo a, WhoInfo b, SortColumnDetails sortColumn) => sortColumn.name == 'user'
-      ? sortColumn.sortDirection == DataGridSortDirection.ascending
-          ? a.user.toLowerCase().compareTo(b.user.toLowerCase())
-          : b.user.toLowerCase().compareTo(a.user.toLowerCase())
-      : super.compare(a, b, sortColumn);
-}
+//   @override
+//   int compare(WhoInfo a, WhoInfo b, SortColumnDetails sortColumn) =>
+//       sortColumn.name == 'user'
+//           ? sortColumn.sortDirection == DataGridSortDirection.ascending
+//               ? a.user.toLowerCase().compareTo(b.user.toLowerCase())
+//               : b.user.toLowerCase().compareTo(a.user.toLowerCase())
+//           : super.compare(a, b, sortColumn);
+
+//   @override
+//   DataGridRowAdapter buildRow(DataGridRow row) {
+//     // TODO: implement buildRow
+//     throw UnimplementedError();
+//   }
+// }
 
 /*
 enum UserState { none, playing, watching }
